baab45362a3b6fd6e729e5cfb1e8a25b0211b560
pipi <luyu8795100@gmail.com>
2026-06-08 11:36:43 +0800

    07-3

--- src/app/zh-TW/hero/[slug]/counters/page.tsx
+++ src/app/zh-TW/hero/[slug]/counters/page.tsx
@@ -1,3 +1,6 @@
 // src/app/zh-TW/hero/[slug]/counters/page.tsx
-export { default } from '../../../hero/[slug]/counters/page';
-export { generateMetadata, generateStaticParams } from '../../../hero/[slug]/counters/page';
+import CounterPage from '../../../hero/[slug]/counters/page';
+import { generateMetadata, generateStaticParams } from '../../../hero/[slug]/counters/page';
+
+export default CounterPage;
+export { generateMetadata, generateStaticParams };
